8203d4b3f1a52dd116904f7bcca4035a7d38855f
Yuche Lin <chivesab@gmail.com>
2021-12-11 13:44:43 -0800

    some commit

--- backend/StockManager.py
+++ backend/StockManager.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 stocks = {
     'Ethical Investing': ["AAPL", "TSLA", "ADBE"],
@@ -11,20 +12,21 @@ stocks = {
 
 
 def get_latest_price(strategies):
-
     latest_price = {}
-    base_url = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/'
-    url_filter = ''
+    base_url = "https://financialmodelingprep.com/api/v3/quote-short/"
+    url_list = []
     for strategy in strategies:
-        url_filter = url_filter + ',' + ','.join(stocks[strategy])
-
-    response = requests.get(base_url + url_filter)
-    if response.status_code != 200:
-        Exception("API Error")
-    response_json = response.json()
-    for item in response_json['companiesPriceList']:
+        for symbol in stocks[strategy]:
+            url_list.append(symbol + "?apikey=3a807ad83b99593ca93f8d0345faf840")
+    for url in url_list:
+        response = requests.get(base_url + url)
+        if response.status_code != 200:
+            Exception("API Error")
+        response_json = response.json()
+        item = response_json[0]
+        #print("item = ",item)
         latest_price[item['symbol']] = {"price": item['price'], "strategy": get_strategy_by_stock(item['symbol'])}
-
+    #print("lastest_price = ",latest_price)
     return latest_price
 
 
@@ -38,7 +40,7 @@ def allocate_stocks(amount, strategies):
 
     # sort by desc price
     latest_price = {k: v for k, v in sorted(latest_price.items(), key=lambda item: item[1]["price"], reverse=True)}
-
+    #print("latest_price in allocate_stocks = ", latest_price)
     change = 0
     per_stock_amount = 0
     if len(latest_price) != 0:
@@ -51,65 +53,28 @@ def allocate_stocks(amount, strategies):
         allocation[ticker] = {"stocks": number_of_stocks, "price": stock_price, "strategy": meta.get("strategy")}
         pie_chart_data.append({"name": ticker, "value": number_of_stocks * stock_price})
 
+    #print({"allocation": allocation, "weekly_trend": get_weekly_trend(strategies, allocation),
+    #        "pie_chart_data": pie_chart_data})
+    print("####### pie_chart = ", pie_chart_data)
     return {"allocation": allocation, "weekly_trend": get_weekly_trend(strategies, allocation),
             "pie_chart_data": pie_chart_data}
 
 
 def get_weekly_trend(strategies, allocation):
-    # "https://financialmodelingprep.com/api/v3/historical-price-full/AAPL,MSFT?from=2019-12-10&to=2019-12-12"
-
-    weekly_trend = {"total": {}}
-    weekly_trend = []
-
+    weekly_trend = {"total": []}
     base_url = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
-    from_date = (datetime.today() - timedelta(days=8)).strftime('%Y-%m-%d')
-    to_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-    date_filter = '?from={}&to={}'.format(from_date, to_date)
-
     for strategy in strategies:
+        #print("strategy = ", strategy)
         for ticker in stocks.get(strategy):
-            response = requests.get(base_url + ticker + date_filter)
+            #print("ticker = ", ticker)
+            url = base_url + ticker + "?timeseries=5&apikey=3a807ad83b99593ca93f8d0345faf840"
+            response = requests.get(url)
             if response.status_code != 200:
                 Exception("API Error")
             response_json = response.json()
-            ticker = response_json['symbol']
-            # weekly_trend[ticker] = {}
-
-            for trend in response_json['historical']:
-                date = trend['date']
-                trend_index = next((weekly_trend.index(x) for x in weekly_trend if x['name'] == date), None)
-                if trend_index is None:
-                    element = {'name': date}
-                    weekly_trend.append(element)
-                    trend_index = weekly_trend.index(element)
-
-                stock_allocation = trend['close'] * allocation.get(ticker).get("stocks")
-
-                # weekly_trend[ticker][date] = stock_allocation
-                weekly_trend[trend_index][ticker] = stock_allocation
-                if weekly_trend[trend_index].get("Total Portfolio") is None:
-                    weekly_trend[trend_index]["Total Portfolio"] = stock_allocation
-                else:
-                    weekly_trend[trend_index]["Total Portfolio"] += stock_allocation
-
-                # if weekly_trend["total"].get(date) is None:
-                #     weekly_trend["total"][date] = stock_allocation
-                # else:
-                #     weekly_trend["total"][date] += stock_allocation
-
-    # add today's portfolio value to weekly trend
-    element = {'name': 'Latest Value'}
-    weekly_trend.append(element)
-    trend_index = weekly_trend.index(element)
-    portfolio_value = 0
-    for ticker, meta in allocation.items():
-        weekly_trend[trend_index][ticker] = meta.get("stocks") * meta.get("price")
-        # weekly_trend.get(ticker)["latest"] = meta.get("stocks") * meta.get("price")
-        portfolio_value += meta.get("stocks") * meta.get("price")
-    # date_today = (datetime.today()).strftime('%Y-%m-%d')
-    # weekly_trend["total"]["latest"] = portfolio_value
-    weekly_trend[trend_index]["Total Portfolio"] = portfolio_value
-
+            weekly_trend["total"].append(response_json)
+    #print("weekly_trend = ", weekly_trend)
+    #print("len(weekly_trend['total']) = ", len(weekly_trend["total"]))
     return weekly_trend
 
 
