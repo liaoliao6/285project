72a9263d2737d82e65b1172d3ab7f54053888169
Yuche Lin <yuche.lin@sjsu.edu>
2021-12-11 17:31:56 -0800

    some commit

--- StockManager.py
+++ StockManager.py
@@ -4,10 +4,10 @@ from collections import defaultdict
 
 stocks = {
     'Ethical Investing': ["AAPL", "TSLA", "ADBE"],
-    'Growth Investing': ["OXLC", "ECC", "AMD"],
-    'Index Investing': ["VOO", "VTI", "ILTB"],
-    'Quality Investing': ["NVDA", "MU", "CSCO"],
-    'Value Investing': ["INTC", "BABA", "GE"]
+    'Growth Investing': ["ABNB", "ASAN", "AMD"],
+    'Index Investing': ["VOO", "VTI", "ARKK"],
+    'Quality Investing': ["NVDA", "TWLO", "CSCO"],
+    'Value Investing': ["INTC", "DIS", "CHWY"]
 }
 
 
@@ -24,9 +24,7 @@ def get_latest_price(strategies):
             Exception("API Error")
         response_json = response.json()
         item = response_json[0]
-        #print("item = ",item)
         latest_price[item['symbol']] = {"price": item['price'], "strategy": get_strategy_by_stock(item['symbol'])}
-    #print("lastest_price = ",latest_price)
     return latest_price
 
 
@@ -40,7 +38,6 @@ def allocate_stocks(amount, strategies):
 
     # sort by desc price
     latest_price = {k: v for k, v in sorted(latest_price.items(), key=lambda item: item[1]["price"], reverse=True)}
-    #print("latest_price in allocate_stocks = ", latest_price)
     change = 0
     per_stock_amount = 0
     if len(latest_price) != 0:
@@ -52,10 +49,6 @@ def allocate_stocks(amount, strategies):
         change = (per_stock_amount + change) - (stock_price * number_of_stocks)
         allocation[ticker] = {"stocks": number_of_stocks, "price": stock_price, "strategy": meta.get("strategy")}
         pie_chart_data.append({"name": ticker, "value": number_of_stocks * stock_price})
-
-    #print({"allocation": allocation, "weekly_trend": get_weekly_trend(strategies, allocation),
-    #        "pie_chart_data": pie_chart_data})
-    print("####### pie_chart = ", pie_chart_data)
     return {"allocation": allocation, "weekly_trend": get_weekly_trend(strategies, allocation),
             "pie_chart_data": pie_chart_data}
 
@@ -64,17 +57,13 @@ def get_weekly_trend(strategies, allocation):
     weekly_trend = {"total": []}
     base_url = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
     for strategy in strategies:
-        #print("strategy = ", strategy)
         for ticker in stocks.get(strategy):
-            #print("ticker = ", ticker)
             url = base_url + ticker + "?timeseries=5&apikey=3a807ad83b99593ca93f8d0345faf840"
             response = requests.get(url)
             if response.status_code != 200:
                 Exception("API Error")
             response_json = response.json()
             weekly_trend["total"].append(response_json)
-    #print("weekly_trend = ", weekly_trend)
-    #print("len(weekly_trend['total']) = ", len(weekly_trend["total"]))
     return weekly_trend
 
 
